1970f6570d29137bb9f7c76f4d5c858f18f5379e
longshengguoji <lsgjzhuwei@163.com>
2017-01-10 18:19:22 +0800

    增加openlayers3学习笔记

---
+++ olStudy/transform.js
@@ -0,0 +1,37 @@
+var PI= 3.14159265358979324;
+/**
+ * 转换WGS84坐标到火星坐标系
+ * @param lat 纬度
+ * @param lon 经度
+ * @returns {}
+ * @autor zhuwei3
+ */
+function transformToGCJ02Fun(lat, lon) {
+    var a = 6378245.0; //  a: 卫星椭球坐标投影到平面地图坐标系的投影因子。
+    var ee = 0.00669342162296594323; //  ee: 椭球的偏心率。
+    var dLat = transformLatFun(lon - 105.0, lat - 35.0);
+    var dLon = transformLonFun(lon - 105.0, lat - 35.0);
+    var radLat = lat / 180.0 * PI;
+    var magic = Math.sin(radLat);
+    magic = 1 - ee * magic * magic;
+    var sqrtMagic = Math.sqrt(magic);
+    dLat = (dLat * 180.0) / ((a * (1 - ee)) / (magic * sqrtMagic) * PI);
+    dLon = (dLon * 180.0) / (a / sqrtMagic * Math.cos(radLat) * PI);
+    return [lat + dLat,lon + dLon];
+}
+
+function transformLatFun(x, y) {
+    var ret = -100.0 + 2.0 * x + 3.0 * y + 0.2 * y * y + 0.1 * x * y + 0.2 * Math.sqrt(Math.abs(x));
+    ret += (20.0 * Math.sin(6.0 * x * PI) + 20.0 * Math.sin(2.0 * x * PI)) * 2.0 / 3.0;
+    ret += (20.0 * Math.sin(y * PI) + 40.0 * Math.sin(y / 3.0 * PI)) * 2.0 / 3.0;
+    ret += (160.0 * Math.sin(y / 12.0 * PI) + 320 * Math.sin(y * PI / 30.0)) * 2.0 / 3.0;
+    return ret;
+}
+
+function transformLonFun(x, y) {
+    var ret = 300.0 + x + 2.0 * y + 0.1 * x * x + 0.1 * x * y + 0.1 * Math.sqrt(Math.abs(x));
+    ret += (20.0 * Math.sin(6.0 * x * PI) + 20.0 * Math.sin(2.0 * x * PI)) * 2.0 / 3.0;
+    ret += (20.0 * Math.sin(x * PI) + 40.0 * Math.sin(x / 3.0 * PI)) * 2.0 / 3.0;
+    ret += (150.0 * Math.sin(x / 12.0 * PI) + 300.0 * Math.sin(x / 30.0 * PI)) * 2.0 / 3.0;
+    return ret;
+}
